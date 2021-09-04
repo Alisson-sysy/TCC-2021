db8444471aa5fb023a990fc5505cf41e89c2efa9
Alisson Garcia da Rosa <56702918+Alisson-sysy@users.noreply.github.com>
2021-09-04 16:50:11 -0300

    Fase 5 - Css Login

--- index.php
+++ index.php
@@ -1,40 +1,57 @@
-<!DOCTYPE html>
 <?php 
     include_once("./settings/connections/direct.php");
     if(isset($_GET["error"]) && $_GET["error"] == "1"){
-        $text = "<br><p class = 'TextError'>Senha ou Login incorretos</p>";
+        $text = "<br><p class = 'TextError'><b>CUIDADO!<b> Senha ou Login incorretos</p>";
     }elseif(isset($_GET["error"]) && $_GET["error"] == "2"){
         $text = "<br><p class = 'TextError'>Sessão expirada/Acesso a página sem autorização</p>";
     }else{
         $text = "";
     }
-    //  include_once("settings/push/pushDatas.php")
-
-    // define('ROOT_PATH', dirname(__FILE__));
 
 ?>
+<!DOCTYPE html>
 
-<head>
-    <title>E.E.E.Chapeuzinho Vermelho</title>
-    <meta charset="UTF-8" />    
-</head>
+<html lang="pt-br">
+    <head>
+        <title>E.E.E.Chapeuzinho Vermelho</title>
+        <meta charset="UTF-8" />   
+        <link rel="stylesheet" href="/TCC/CSS/login/cssIndex.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Londrina+Sketch&family=Poppins:wght@100&display=swap" rel="stylesheet">
+    </head>
 
-<body>
-    <center>
-    <form action="settings/connections/login.php" method="POST">
+    <body>
+        <center>
+        <main class="mainCount">
 
-        <label for="login">login</label>
-        <input type="text" name="login" id="login" placeholder="Exemplo@gmail.com">
-        <br>
+            <div class="infoSis">
+                <div class="imgLogo">
+                    <img src="/TCC/CSS/imgCss/Logo.png" alt="" class="logoIm">
+                </div>
+                <div class="textLogo">
+                    <p class="logoTittle">Profie</p>
+                    <p class='logoTetx'>Seu sistema</p>
+                    <p class='logoTetx'>de organização</p>
+                </div>
+            </div>
 
-        <label for="password">Senha:</label>
-        <input type="password" name="password" id="password" placeholder="Digite sua senha aqui">
-        <br>
+            <div class="inputs">
+                <form action="settings/connections/login.php" method="POST">
 
-        <input type="submit" value="Login">
-        <?php echo $text?>
+                    <input type="text" name="login" class='inputLogin' id="login" placeholder="Digite seu Login aqui">
+                    <br>
 
-    </form>
-</body>
+                    <input type="password" name="password" class='inputLogin' id="password"  placeholder="Digite sua senha aqui">
+                    <br>
+
+                    <input type="submit" class='submitButton' class='Modificar_Login' value="Login">
+                    <?php echo $text?>
+                </form>
+            </div>
+            
+        </main>
+    </body>
 
 </html>
+
--- visualization/Troca_senha.php
+++ visualization/Troca_senha.php
@@ -4,20 +4,45 @@
     include_once('../settings/connections/check_session.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/TCC/CSS/login/mudaLogin.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Londrina+Sketch&family=Poppins:wght@100&display=swap" rel="stylesheet">
+        <title>Document</title>
+    </head>
+    <body>
+        <center>
+         <!-- Formulário -->
+        <li><a href="../settings/connections/exit.php">Sair</a></li>
+        <main class="mainCount">
+            <div class="textLogo">
+                <p class="logoTittle">Profie</p>
+                <p class="logoTetx">
+                    Para começarmos a utilização do sistema,
+                </p>
+                <p class="logoTetx">
+                    é preciso que você insira suas novas informações de login!
+                </p>
+            </div>
 
- <!-- Formulário -->
- <li><a href="../settings/connections/exit.php">Sair</a></li>
- <form action="../settings/push/pushDatas.php" method="POST">
+            <form action="../settings/push/pushDatas.php" class='form' method="POST">
+                    <div class="Novo_login">
+                        <input type="text" name='login' class='inputLogin'  placeholder='Seu novo login' required>
+                    </div>
 
-<div class="Novo_login">
-    <label>Novo Login</label>
-    <input type="text" name='login' required>
-</div>
-<div class='password'>
-    <label>Nova senha</label>
-    <input type="password" name='password' required>
-</div>
+                    <div class='password'>
+                        <input type="password" name='password' class='inputLogin' placeholder='Sua nova senha' required>
+                    </div>
 
-<input type="submit" value="Modificar_Login" name="acao">
-
-</form>
+                    <input type="hidden" name="acao" value='Modificar_Login'>
+                    <input type="submit" class="submitButton"  value="Pronto" >
+            </form>
+        </main>
+    </body>
+</html>
